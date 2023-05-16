4db052202c58ec1e9105faca95cee6645582439b
RadekWoz <rwozniak89@gmail.com>
2023-05-16 12:42:40 +0200

    Login

--- IdentityAppMVC/Controllers/AccountController.cs
+++ IdentityAppMVC/Controllers/AccountController.cs
@@ -22,6 +22,59 @@ namespace IdentityAppMVC.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            LoginViewModel loginViewModel = new LoginViewModel();
+            loginViewModel.ReturnUrl = returnUrl ?? Url.Content("~/");
+            return View(loginViewModel);
+        }
+
+
+        /// https://stackoverflow.com/questions/36383768/identity-3-signinmanager-passwordsigninasync-doesnt-return-any-result
+        //var user = await _userManager.FindByEmailAsync(model.Email);
+        //var password = await _userManager.CheckPasswordAsync(user, model.Password);
+
+        //if(password)
+        //{
+        //   var result = await _signInManager.SignInAsync(
+        //                    model.Email,
+        //                    model.Password,
+        //                    model.RememberMe);
+        //     if (result.Succeeded)
+        //     {
+        //          return RedirectToAction(nameof(EmployeeController.Contact), "Employee");
+        //    }
+        //}       
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel loginViewModel, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(loginViewModel.UserName, loginViewModel.Password, loginViewModel.RememberMe, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return View(loginViewModel);
+                }
+            }
+            return View(loginViewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> LogOff()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
         public async Task<IActionResult> Register(string? returnUrl = null)
         {
             RegisterViewModel registerViewModel = new RegisterViewModel();
